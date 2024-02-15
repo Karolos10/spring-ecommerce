9cccfbf237166dc4a46551bd2c14f26b65f6c540
Karlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-02-14 23:07:16 -0500

    implement a different template for a logged-in user

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -59,6 +59,9 @@ public class HomeController {
 		log.info("session del usuario: {}", session.getAttribute("idusuario"));
 		
 		model.addAttribute("productos", productoService.findAll());
+		
+		//session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "usuario/home";
 	}
 	
@@ -136,10 +139,13 @@ public class HomeController {
 	}
 	
 	@GetMapping("/getCart")
-	public String getCart(Model model) {
+	public String getCart(Model model, HttpSession session) {
 		
 		model.addAttribute("cart", detalles);
 		model.addAttribute("orden", orden);
+		
+		//session
+		model.addAttribute("sesion", session.getAttribute("idusuario"));
 		return "/usuario/carrito";
 	}
 	
