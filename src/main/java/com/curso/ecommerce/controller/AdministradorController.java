9999cbf33545571234f9c5dcb0d499b585880421
Karlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-02-16 13:34:48 -0500

    The functionality to view order details in the admin user

--- src/main/java/com/curso/ecommerce/controller/AdministradorController.java
+++ src/main/java/com/curso/ecommerce/controller/AdministradorController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.IOrdenService;
 import com.curso.ecommerce.service.IUsuarioService;
@@ -49,6 +51,15 @@ public class AdministradorController {
 		
 		model.addAttribute("ordenes", ordenService.findAll());
 		return "administrador/ordenes";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable Integer id) {
+		
+		Orden orden = ordenService.findById(id).get();
+		
+		model.addAttribute("detalles", orden.getDetalle());
+		return "administrador/detalleorden";
 	}
 
 }
